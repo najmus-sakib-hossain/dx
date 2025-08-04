b6e6603e76e885ebe27da65133e9fed914d8f262
najmus-sakib-hossain <manfromexistence@proton.me>
2025-08-04 17:26:53 +0000

    fix: add missing button class for consistent styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="flex body" id="f1">
-      <button className="" id="b1">Click me</button>
+      <button className="button" id="b1">Click me</button>
     </div>
   );
 }
