4a20b620a8f1b9be5cfa9ef0e42aeba5c915f298
najmus-sakib-hossain <manfromexistence@proton.me>
2025-08-04 16:44:48 +0000

    refactor: update button ID and clean up styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="flex" id="f1">
-      <button className="button btn" id="bb1">Click me</button>
+      <button className="button" id="b1">Click me</button>
     </div>
   );
 }
