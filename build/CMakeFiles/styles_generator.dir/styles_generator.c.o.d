3a607ceefea4c8aaec639193934d0b499149d900
najmus-sakib-hossain <manfromexistence@proton.me>
2025-08-04 14:00:02 +0000

    feat: hi

---
+++ build/CMakeFiles/styles_generator.dir/styles_generator.c.o.d
@@ -0,0 +1,87 @@
+CMakeFiles/styles_generator.dir/styles_generator.c.o: \
+ /workspaces/dx/styles_generator.c /usr/include/stdc-predef.h \
+ /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/local/include/tomlc99/toml.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /usr/local/include/flatcc/flatcc_builder.h \
+ /usr/local/include/flatcc/flatcc_flatbuffers.h \
+ /usr/local/include/flatcc/portable/pstatic_assert.h \
+ /usr/include/assert.h /usr/local/include/flatcc/portable/pwarnings.h \
+ /usr/local/include/flatcc/portable/pstdalign.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdalign.h \
+ /usr/local/include/flatcc/portable/paligned_alloc.h \
+ /usr/local/include/flatcc/portable/pattributes.h \
+ /usr/local/include/flatcc/flatcc_alloc.h \
+ /usr/local/include/flatcc/portable/paligned_alloc.h \
+ /usr/local/include/flatcc/flatcc_assert.h \
+ /usr/local/include/flatcc/portable/pendian.h \
+ /usr/local/include/flatcc/portable/pendian_detect.h \
+ /usr/local/include/flatcc/portable/pmemaccess.h \
+ /usr/local/include/flatcc/flatcc_types.h \
+ /usr/local/include/flatcc/flatcc_endian.h \
+ /usr/local/include/flatcc/flatcc_accessors.h \
+ /usr/local/include/flatcc/flatcc_identifier.h \
+ /usr/local/include/flatcc/flatcc_emitter.h \
+ /usr/local/include/flatcc/flatcc_iov.h \
+ /usr/local/include/flatcc/flatcc_refmap.h \
+ /workspaces/dx/styles_generated.h \
+ /usr/local/include/flatcc/flatcc_prologue.h \
+ /usr/local/include/flatcc/portable/pdiagnostic_push.h \
+ /usr/local/include/flatcc/portable/pdiagnostic.h \
+ /usr/local/include/flatcc/flatcc_flatbuffers.h \
+ /usr/local/include/flatcc/flatcc_epilogue.h \
+ /usr/local/include/flatcc/portable/pdiagnostic_pop.h \
+ /usr/local/include/flatcc/flatcc_verifier.h
