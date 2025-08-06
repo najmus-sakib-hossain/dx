61d801f06d462baca8e7d6a369a1f70114efc719
najmus-sakib-hossain <manfromexistence@proton.me>
2025-08-06 08:28:24 +0000

    feat: dx-styles is fastest!!!

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="flex body" id="fb">
+    <div className="flex body hidden" id="fbh">
       <button className="button flex" id="bf">Click me</button>
     </div>
   );
